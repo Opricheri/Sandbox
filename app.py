054c7189cdf393c31438795cb266ad7d236224ce
Opricheri <108458245+Opricheri@users.noreply.github.com>
2022-07-03 15:56:10 +0900

    change

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, redirect
+import re
 
-app = Flask(__name__, static_folder='./templates/images')
+app = Flask(__name__, static_folder='./static/css')
 
 @app.route('/')
 def sandbox():
     return render_template('sandbox.html')
+
+@app.route('/2')
+def page2():
+    return "ここは２ページ。"
 
 
 if __name__ == '__main__':

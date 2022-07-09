618a40a7858550bfdc770ec387a68888dd1df0d2
Opricheri <secrettail245525@gmail.com>
2022-07-09 13:18:20 +0900

    some change

--- static/js/sandbox.js
+++ static/js/sandbox.js
@@ -12,4 +12,11 @@ function countClick() {
     cnt ++;
     let element = document.getElementById("count");
     element.textContent = String(cnt);
+    if (cnt === 50) {
+        element.textContent = String(cnt + "   「がんばれー！」");
+    }
+    if (cnt === 100) {
+        element.textContent = String("神は言っている。FGOをやれと。")
+        location = "https://www.fate-go.jp";
+    }
 }

9f6b1e7f3ae3485e07eeff76024538ab95916677
Opricheri <secrettail245525@gmail.com>
2022-07-18 23:18:35 +0900

    css

--- static/js/like_newspaper.js
+++ static/js/like_newspaper.js
@@ -1,30 +1,122 @@
 //テキストのカウントアップ+バーの設定
-var bar = new ProgressBar.Line(splash_text, {//id名を指定
-	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-	duration: 10,//時間指定(1000＝1秒)
-	strokeWidth: 0.5,//進捗ゲージの太さ
-	color: '#555',//進捗ゲージのカラー
-	trailWidth: 0.2,//ゲージベースの線の太さ
-	trailColor: '#bbb',//ゲージベースの線のカラー
-	text: {//テキストの形状を直接指定				
-		style: {//天地中央に配置
+var bar = new ProgressBar.Line(splash_text, { //id名を指定
+	easing: 'easeInOut', //アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	duration: 10, //時間指定(1000＝1秒)
+	strokeWidth: 0.5, //進捗ゲージの太さ
+	color: '#555', //進捗ゲージのカラー
+	trailWidth: 0.2, //ゲージベースの線の太さ
+	trailColor: '#bbb', //ゲージベースの線のカラー
+	text: { //テキストの形状を直接指定				
+		style: { //天地中央に配置
 			position: 'absolute',
 			left: '50%',
 			top: '50%',
 			padding: '0',
-			margin: '-30px 0 0 0',//バーより上に配置
-			transform:'translate(-50%,-50%)',
-			'font-size':'1rem',
+			margin: '-30px 0 0 0', //バーより上に配置
+			transform: 'translate(-50%,-50%)',
+			'font-size': '1rem',
 			color: '#fff',
 		},
 		autoStyleContainer: false //自動付与のスタイルを切る
 	},
-	step: function(state, bar) {
+	step: function (state, bar) {
 		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
 	}
 });
 
 //アニメーションスタート
-bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
-	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
-});  
+bar.animate(1.0, function () { //バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash").delay(500).fadeOut(800); //アニメーションが終わったら#splashエリアをフェードアウト
+});
+
+
+
+
+
+
+//スクロールをするとハンバーガーメニューに変化するための設定を関数でまとめる
+function FixedAnime() {
+	//ヘッダーの高さを取得
+	var headerH = $('#header').outerHeight(true);
+	var scroll = $(window).scrollTop();
+	if (scroll >= headerH) { //ヘッダーの高さ以上までスクロールしたら
+		$('.openbtn').addClass('fadeDown'); //.openbtnにfadeDownというクラス名を付与して
+		$('#header').addClass('dnone'); //#headerにdnoneというクラス名を付与
+	} else { //それ以外は
+		$('.openbtn').removeClass('fadeDown'); //fadeDownというクラス名を除き
+		$('#header').removeClass('dnone'); //dnoneというクラス名を除く
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	FixedAnime(); //スクロールをするとハンバーガーメニューに変化するための関数を呼ぶ
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	FixedAnime(); //スクロールをするとハンバーガーメニューに変化するための関数を呼ぶ
+});
+
+
+//ボタンをクリックした際のアニメーションの設定
+$(".openbtn").click(function () { //ボタンがクリックされたら
+	$(this).toggleClass('active'); //ボタン自身に activeクラスを付与し
+	$("#header").toggleClass('panelactive'); //ヘッダーにpanelactiveクラスを付与
+});
+$("#g-navi li a").click(function () { //ナビゲーションのリンクがクリックされたら
+	$(".openbtn").removeClass('active'); //ボタンの activeクラスを除去し
+	$("#header").removeClass('panelactive'); //ヘッダーのpanelactiveクラスも除去
+});
+
+
+/*リンク先のidまでスムーススクロール
+//※ページ内リンクを行わない場合は不必要なので削除してください
+    $('#g-navi li a').click(function () {
+	var elmHash = $(this).attr('href');
+	var pos = $(elmHash).offset().top-0;
+	$('body,html').animate({scrollTop: pos}, 1000);
+	return false;
+});
+*/
+
+
+
+
+
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200) { //上から200pxスクロールしたら
+		$('#page-top').removeClass('DownMove'); //#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove'); //#page-topについているUpMoveというクラス名を付与
+	} else {
+		if ($('#page-top').hasClass('UpMove')) { //すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove'); //UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove'); //DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+	$('body,html').animate({
+		scrollTop: 0 //ページトップまでスクロール
+	}, 500); //ページトップスクロールの速さ。数字が大きいほど遅くなる
+	return false; //リンク自体の無効化
+});
+
+
+
+
+
+
